9e2648ff22e30c48e9e1e76d0f78430810d96e84
Ekaterinaegrv <ekaterinaegrv@gmail.com>
2023-03-27 14:25:31 +0300

    dashboard bug fix

--- client/src/pages/Dashboard/Dashboard.js
+++ client/src/pages/Dashboard/Dashboard.js
@@ -4,33 +4,43 @@ import CONSTANTS from '../../constants';
 import CustomerDashboard from '../../components/CustomerDashboard/CustomerDashboard';
 import CreatorDashboard from '../../components/CreatorDashboard/CreatorDashboard';
 import Header from '../../components/Header/Header';
+import LoginPage from '../LoginPage/LoginPage';
+import LoginForm from '../../components/LoginForm/LoginForm';
+import styles from '../LoginPage/LoginPage.module.sass'
 
 const Dashboard = (props) => {
   const { role, history } = props;
-
+  console.log(role);
 
   const dashboardRender = () => {
-    switch(role) {
-      case CONSTANTS.CUSTOMER:{
-        return (<CustomerDashboard history={history} match={props.match} />)
-      }
-      case CONSTANTS.CREATOR:{
-        return (<CreatorDashboard history={history} match={props.match} />)
-      }
-      case CONSTANTS.MODERATOR:{
-        return (<CustomerDashboard history={history} match={props.match} role={props.role}/>)
-      }
-       
-      }
-  }
-  
+    if(role) {
+      switch(role) {
+        case CONSTANTS.CUSTOMER:{
+          return (<CustomerDashboard history={history} match={props.match} />)
+        }
+        case CONSTANTS.CREATOR:{
+          return (<CreatorDashboard history={history} match={props.match} />)
+        }
+        case CONSTANTS.MODERATOR:{
+          return (<CustomerDashboard history={history} match={props.match} role={props.role}/>)
+        } }
+    } else {
+        return (
+          <div className={styles.mainContainer}>
+          <div className={styles.loginContainer}>
+            <div className={styles.loginFormContainer}>
+              <LoginForm history={history} />
+            </div>
+          </div>
+        </div>
+        )
+    }
+  } 
 
   return (
     <div>
       <Header />
-      {dashboardRender()}
-               
-               
+      {dashboardRender()}     
     </div>
   );
 };
